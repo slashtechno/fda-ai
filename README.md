bd05128ffcf010dadef96d91783982475a6615ef
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-08-28 15:35:38 -0500

    feat: Convert Unstructured docs to Langchain docs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rag-testing  
 A learning project for RAG!  
+
+`simple/` is not necessarily simple, but it doesn't depend on cloud APIs  
+`graphlit/` was an attempt at using [Graphlit](https://www.graphlit.com/) which is RAG-as-a-service  
--- simple/rag_testing/load.py
+++ simple/rag_testing/load.py
@@ -1,15 +1,19 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
+from unstructured.partition.pdf import partition_pdf
 from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.schema.document import Document
+from langchain.schema.document import Document as DocumentSchema
+from langchain_core.documents import Document
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 from pathlib import Path
+from unstructured.chunking.title import chunk_by_title
+
 from langchain_community.vectorstores.utils import filter_complex_metadata
 CHROMA_PATH = "chroma"
 DATA_PATH = "data"
 
-
+skip = ["210951Orig1s000MultidisciplineR.pdf"]
 
 # https://python.langchain.com/v0.2/docs/integrations/providers/unstructured/
 # https://python.langchain.com/v0.2/docs/integrations/document_loaders/unstructured_file/
@@ -18,15 +22,19 @@ def load_documents():
     # return UnstructuredLoader(file_path=files, chunking_strategy="by_title", max_characters=2200, new_after_n_chars=1000, overlap=300).load()
     docs = []
     for file in Path(DATA_PATH).rglob("*.pdf"):
+        if file.name in skip:
+            continue
         print(f"Loading {file}")
-        docs.extend(UnstructuredPDFLoader(file_path=file, mode="elements", infer_table_structure=True, ).load())
+        # doc_loader = UnstructuredPDFLoader(file_path=file, mode="single", infer_table_structure=True,  strategy="hi_res", show_progress_bar=True) 
+        # docs.extend(doc_loader.load())
+        docs.extend(partition_pdf(filename=file, infer_table_structure=True,  strategy="hi_res", show_progress_bar=True))
     return docs
 
     # document_loader = PyPDFDirectoryLoader(DATA_PATH)
     # return document_loader.load()
 
 
-def split_documents(documents: list[Document]):
+def split_documents(documents: list[DocumentSchema]):
     text_splitter = RecursiveCharacterTextSplitter(
         # Don't split by any separators
         separators=[],
@@ -50,7 +58,7 @@ def get_embedding_function():
     return embeddings
 
 
-def add_to_chroma(chunks: list[Document]):
+def add_to_chroma(chunks: list[DocumentSchema]):
     # db = Chroma.from_documents(
     #         new_chunks,
     #         embedding=get_embedding_function(),
@@ -60,6 +68,7 @@ def add_to_chroma(chunks: list[Document]):
         persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
     )
 
+    # TODO: Edit calculate_chunk_ids to use metadata from Unstructured
     chunks_with_ids = calculate_chunk_ids(chunks)
 
     # IDs are always included
@@ -86,7 +95,7 @@ def add_to_chroma(chunks: list[Document]):
         print("No new documents to add")
 
 
-def calculate_chunk_ids(chunks: list[Document]):
+def calculate_chunk_ids(chunks: list[DocumentSchema]):
     last_page_id = None
     current_chunk_index = 0
     for chunk in chunks:
@@ -104,9 +113,25 @@ def calculate_chunk_ids(chunks: list[Document]):
     return chunks
 
 
+def chunk_pdf(documents: list[DocumentSchema]):
+    # https://colab.research.google.com/drive/1ieDJ4LoxARrHFqxXWif8Lv8e8aZTgmtH#scrollTo=f5a68771a12d16e6
+    chunked_elements = chunk_by_title(documents, max_characters=3000, new_after_n_chars=2000, overlap=300)
+    documents = []
+    for element in chunked_elements:
+        metadata = element.metadata.to_dict()
+        documents.append(
+            Document(
+                page_content=element.text,
+                metadata=metadata,
+            )
+        )
+    return documents    
+
+
 def main():
     documents = load_documents()
-    chunks = documents # Unstructured can split the documents at load time
+    chunks = chunk_pdf(documents)
+    # chunks = documents # Unstructured can split the documents at load time
     # chunks = split_documents(documents)
     # print(chunks[0])
     add_to_chroma(chunks)
